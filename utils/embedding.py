03aa6d7dcdc5ba083a91034eb04634696ea66335
damien <yithanglee@gmail.com>
2025-12-12 10:19:21 +0000

    fix

--- utils/embedding.py
+++ utils/embedding.py
@@ -6,5 +6,5 @@ def get_embedding(img):
     img = cv2.resize(img, (224, 224))
     img = img.astype(np.float32) / 255.0
     x = np.transpose(img, (2,0,1))[None, ...]
-    emb = session.run(None, {"input": x})[0]
+    emb = session.run(None, {"x": x})[0]
     return emb / np.linalg.norm(emb)
